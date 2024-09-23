edb944f22f585c85e49dffca5431932d48d02dd1
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-09-23 18:07:38 +0700

    macam macam variabel pada dart
    contoh penggunaan variabel pada dart

---
+++ variabel.dart
@@ -0,0 +1,21 @@
+void main() {
+  //variabel string
+  var hallo = "hallo selamat datang di dart language";
+  print(hallo);
+
+  //variabel int
+  var umur;
+  umur = 19;
+  print(umur);
+
+  //variabel double(pecahan)
+  var pecahan = 3.14;
+  print(pecahan);
+
+  //variabel dynamic (dapat berubah ubah tipe datanya)
+  var dynamicc; //variabel dynamic
+  dynamicc = 17;
+  print(dynamicc);
+  print(
+      dynamicc = "ini dynamic variabel"); //tipe data berubah dari int ke string
+}
