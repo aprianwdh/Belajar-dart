68319e089fd4c079d6b858118acc0392a0615a1b
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-09-23 19:26:24 +0700

    bollean pada dart
    pengenalan boolean(true atau false) pada dart

---
+++ bolean.dart
@@ -0,0 +1,18 @@
+void main() {
+  bool selalu_true = true;
+  bool selalu_false = false;
+  bool not_true = !true; //artinya tidak true dengan kata lain FALSE
+  bool not_false = !false; //tidak false artinya TRUE
+
+  print(selalu_false);
+  print(selalu_true);
+  print(not_true);
+  print(not_false);
+
+  if (16 > 10) {
+    //akan mengecek apakah 16 lebih besar dari 10
+    print("ini true"); //jika true
+  } else {
+    print("ini false"); //jika false
+  }
+}
