4f698e8604e6508d7948fd05bbf30b5e3611f18e
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-09-23 19:27:56 +0700

    pengenalan tring pada dart
    contoh penggunaan string pada dart

---
+++ string.dart
@@ -0,0 +1,16 @@
+void main() {
+  //string singgle quote ('')
+  print('ini singgle quote');
+
+  //string double quote("")
+  print("ini double quote");
+
+  //menggunakan variabel ke dalam program ($nama_variabel)
+  String nama = "apriw";
+  print('halo namaku $nama salam kenal');
+
+  // mengunakan back slash agar special signs dapat dibaca sebagai string
+  print(
+      "dia berkata \"aku akan pensiun ketika aku meliaht pemain yang lebih hebat\"");
+  print('aku membeli mobil seharga \$999999');
+}
