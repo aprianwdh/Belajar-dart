01690af624dc0994f5135aa2f01fb8ad6fdc1619
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-09-25 17:42:11 +0700

    percabangan switch case
    contoh percabangan switch case

---
+++ control flow/switch_case.dart
@@ -0,0 +1,16 @@
+void main() {
+  var bilangan1 = 12;
+  var bilangan2 = 15;
+  var operatorr = "-";
+
+  switch (operatorr) {
+    case "+":
+      print("$bilangan1 + $bilangan2 = ${bilangan1 + bilangan2}");
+    case "-":
+      print("$bilangan1 - $bilangan2 = ${bilangan1 - bilangan2}");
+    case "/":
+      print("$bilangan1 / $bilangan2 = ${bilangan1 / bilangan2}");
+    case "*":
+      print("$bilangan1 * $bilangan2 = ${bilangan1 * bilangan2}");
+  }
+}
